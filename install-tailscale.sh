6b88dae64eafd04bf5ed6bb63a9abdb566e01db6
neogerm <neogerm@gmail.com>
2026-03-09 22:28:18 +0100

    Fix: hardcode version 1.94.2 instead of dynamic fetch

--- install-tailscale.sh
+++ install-tailscale.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -x
-TAILSCALE_VERSION=${TAILSCALE_VERSION:-$(curl -sL https://pkgs.tailscale.com/stable/VERSION | tr -d '\n\r')}
+TAILSCALE_VERSION=${TAILSCALE_VERSION:-1.94.2}
 TS_FILE=tailscale_${TAILSCALE_VERSION}_amd64.tgz
 wget -q "https://pkgs.tailscale.com/stable/${TS_FILE}" && tar xzf "${TS_FILE}" --strip-components=1
 cp -r tailscale tailscaled /render/
