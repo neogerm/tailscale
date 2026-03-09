d54aeb8b1aa1ae26ed5c7e3b851c9a8db39c0097
neogerm <neogerm@gmail.com>
2026-03-09 22:24:03 +0100

    Fix: strip newline from VERSION output

--- install-tailscale.sh
+++ install-tailscale.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -x
-TAILSCALE_VERSION=${TAILSCALE_VERSION:-$(curl -sL https://pkgs.tailscale.com/stable/VERSION)}
+TAILSCALE_VERSION=${TAILSCALE_VERSION:-$(curl -sL https://pkgs.tailscale.com/stable/VERSION | tr -d '\n\r')}
 TS_FILE=tailscale_${TAILSCALE_VERSION}_amd64.tgz
 wget -q "https://pkgs.tailscale.com/stable/${TS_FILE}" && tar xzf "${TS_FILE}" --strip-components=1
 cp -r tailscale tailscaled /render/
